6cff7e04e3ba707c789189f528027c56f09ef3f3
redperiabras <redperiabras@gmail.com>
2024-06-18 11:04:59 +0800

    Add proxy

--- phzipcodes/settings.py
+++ phzipcodes/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = "phzipcodes.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -55,6 +55,7 @@ DOWNLOADER_MIDDLEWARES = {
     "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
     "scrapy_fake_useragent.middleware.RetryUserAgentMiddleware": 401,
     "scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware": 1,
+    "scrapy_proxies.RandomProxy": 100,
 }
 
 FAKEUSERAGENT_PROVIDERS = [
@@ -77,26 +78,37 @@ FAKEUSERAGENT_PROVIDERS = [
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = "httpcache"
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 3
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504]
+
+PROXY_LIST = "./proxies.txt"
+
+PROXY_MODE = 0
+
+LOG_LEVEL = "DEBUG"
--- phzipcodes/spiders/phzipcode.py
+++ phzipcodes/spiders/phzipcode.py
@@ -10,35 +10,39 @@ class PhzipcodeSpider(scrapy.Spider):
 
     def parse(self, response):
         regions = response.xpath(
-            '//h1[@class="page-header"]/../div[last()]/div/div/a[0]/@href'
+            '//h1[@class="page-header"]/../div[last()]/div/div/a/@href'
         ).getall()
 
+        # print(regions)
+        
         for path in regions:
             yield Request(url=urlparse.urljoin(response.url, path),callback=self.parse_region)
 
     def parse_region(self, response):
         areas = response.xpath(
-            '//h1[@class="page-header"]/../div[last()]/div/div/a[0]/@href'
+            '//h1[@class="page-header"]/../div[last()]/div/div/a/@href'
         ).getall()
 
+        # print(areas)
+        
         for path in areas:
             yield Request(
                 url=urlparse.urljoin(response.url, path), callback=self.parse_area
             )
 
     def parse_area(self, response):
-
-        rows = response.xpath(
-            "//h1[contains(text(), 'Browse ZIP Code')]/../div/div/table/tbody/tr[position() > 1]"
-        ).getall()
-
-        for row in rows:
+        locations = response.xpath(
+            "//h1[contains(text(), 'Browse ZIP Code')]/../div/div/table/tr[position() > 1]"
+        )
+        # print(locations)
+        
+        for row in locations:
             yield {
-                "location": row.xpath("/td[1]/strong/a/text()").get(),
-                "municipality": row.xpath("/td[2]/strong/a/text()").get(),
-                "province": row.xpath("/td[3]/strong/a/text()").get(),
-                "region": row.xpath("/td[4]/strong/a/text()").get(),
-                "zip_code": row.xpath("/td[5]/strong/a/text()").get()
+                "location": row.xpath("./td[1]/strong/a/text()").get(),
+                "municipality": row.xpath("./td[2]/strong/a/text()").get(),
+                "province": row.xpath("./td[3]/strong/a/text()").get(),
+                "region": row.xpath("./td[4]/strong/a/text()").get(),
+                "zip_code": row.xpath("./td[5]/strong/a/text()").get()
             }
 
         next_page = response.xpath("//div/ul[@class='pagination']/li[last()-1]/a/@href").get()
